02ddb0fc4a7a1a73cb8590bc34034093a94f69ab
mingyu03-kim <kimmingyu65@gmail.com>
2024-08-19 22:14:29 +0900

    🔨 my project code

--- react-project-1/src/App.jsx
+++ react-project-1/src/App.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viewer />
+        <Viewer count={count} />
       </section>
       <section>
-        <Controller />
+        <Controller count={count} setCount={setCount} />
       </section>
     </div>
   );
--- react-project-1/src/components/Controller.jsx
+++ react-project-1/src/components/Controller.jsx
@@ -1,12 +1,16 @@
-const Controller = () => {
+const Controller = ({ count, setCount }) => {
+  const onClickButton = (value) => () => {
+    setCount(count + value);
+  };
+
   return (
     <div>
-      <button>-1</button>
-      <button>-10</button>
-      <button>-100</button>
-      <button>+100</button>
-      <button>+10</button>
-      <button>+1</button>
+      <button onClick={onClickButton(-1)}>-1</button>
+      <button onClick={onClickButton(-10)}>-10</button>
+      <button onClick={onClickButton(-100)}>-100</button>
+      <button onClick={onClickButton(100)}>+100</button>
+      <button onClick={onClickButton(10)}>+10</button>
+      <button onClick={onClickButton(1)}>+1</button>
     </div>
   );
 };
