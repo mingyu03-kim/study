27c8755a7cfea988b1b14c1f2dbf931772eb8d4e
mingyu03-kim <kimmingyu65@gmail.com>
2024-08-27 21:28:03 +0900

    :hammer: use ref.current++ to make ID for list

--- react-project-2/src/App.jsx
+++ react-project-2/src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
 import "./App.css";
 
+const mockData = [
+  { id: 0, isDone: false, content: "React 1", date: new Date().getTime() },
+  { id: 1, isDone: false, content: "React 2", date: new Date().getTime() },
+  { id: 2, isDone: false, content: "React 3", date: new Date().getTime() },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+  };
 
   return (
     <>
       <div className="App">
         <Header />
-        <Editor />
-        <List />
+        <Editor onCreate={onCreate} />
+        <List list={todos} />
       </div>
     </>
   );
