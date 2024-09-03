dcd8552ec3c61400d7320213827c53a1041ddd23
mingyu03-kim <kimmingyu65@gmail.com>
2024-09-03 20:43:14 +0900

    ✨ useMemo == vue computed. useMemo(callback, deps)

--- react-project-2/src/components/List.jsx
+++ react-project-2/src/components/List.jsx
@@ -1,6 +1,6 @@
 import TodoItem from "./TodoItem";
 import "./List.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 const List = ({ todos, onUpdate, onDelete }) => {
   const [search, setSearch] = useState("");
@@ -16,9 +16,44 @@ const List = ({ todos, onUpdate, onDelete }) => {
     );
   };
 
+  // const getAnalyzedData = () => {
+  //   console.log("getAnalyzedData");
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((td) => td.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return {
+  //     totalCount,
+  //     doneCount,
+  //     notDoneCount,
+  //   };
+  // };
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  // 두번째 배열은 deps, 의존성 배열이다.
+  // 즉 두번째 인자의 배열에 들어가 있는 값들이 변경되었을 때만,
+  // 첫번째 인자로 받은 콜백 함수를 실행한다.
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedData");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((td) => td.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {
+      totalCount,
+      doneCount,
+      notDoneCount,
+    };
+  }, [todos]);
+
   return (
     <div className="List">
       <h4>Todo List 😂</h4>
+      <div>
+        <div>total: {totalCount}</div>
+        <div>done: {doneCount} </div>
+        <div>notDone: {notDoneCount}</div>
+      </div>
       <input
         placeholder="검색어를 입력하세요"
         value={search}
