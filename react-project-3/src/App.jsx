1478b23c59887b3d774371e7cc4e075088df0537
mingyu03-kim <kimmingyu65@gmail.com>
2024-09-13 20:48:33 +0900

    🔨 Link = router-view, useNavigate = router.push

--- react-project-3/src/App.jsx
+++ react-project-3/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
@@ -9,13 +9,31 @@ import NotFound from "./pages/NotFound";
 // 2. /new : 새로운 일기 작성
 // 3. /diary : 일기 상세 조회 페이지
 function App() {
+  // navigate로 이동할 수도 있다. (vue의 router.push 방식으로 보여짐)
+  const nav = useNavigate();
+  const onClickButton = () => {
+    nav("/new");
+  };
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/diary" element={<Diary />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+
+        {/* a 태그는 페이지의 깜빡임이 존재 => SPA 방식이 아니다. */}
+        {/* <a href="/">Home</a>
+        <a href="/new">New</a>
+        <a href="/diary">DIary</a> */}
+        <button onClick={onClickButton}>New 페이지로 이동</button>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
