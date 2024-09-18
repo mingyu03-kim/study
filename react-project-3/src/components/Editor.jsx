2a013b445c88b1f72d6cc5acd90ad66405045482
mingyu03-kim <kimmingyu65@gmail.com>
2024-09-18 20:43:16 +0900

    🔨 new / editor page

--- react-project-3/src/components/Editor.jsx
+++ react-project-3/src/components/Editor.jsx
@@ -1,7 +1,8 @@
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const emotionList = [
   {
     emotionId: 1,
@@ -25,14 +26,56 @@ const emotionList = [
   },
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+const getStringedDate = (targetDate) => {
+  // 날짜 -> 포매팅
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
 
+  if (month < 10) month = `0${month}`;
+  if (date < 10) date = `0${date}`;
+
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ onSubmit, initData }) => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 1,
+    content: "",
+  });
+
+  const nav = useNavigate();
+
+  useEffect(() => {
+    if (initData)
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate)),
+      });
+  }, [initData]);
+
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") value = new Date(value);
+    setInput({ ...input, [name]: value });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          type="date"
+          value={getStringedDate(input.createdDate)}
+          onChange={onChangeInput}
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
@@ -40,9 +83,17 @@ const Editor = () => {
           {emotionList.map((item) => {
             return (
               <EmotionItem
+                onClick={() =>
+                  onChangeInput({
+                    target: {
+                      name: "emotionId",
+                      value: item.emotionId,
+                    },
+                  })
+                }
                 key={item.emotionId}
                 {...item}
-                isSelected={item.emotionId === emotionId}
+                isSelected={item.emotionId === input.emotionId}
               />
             );
           })}
@@ -50,11 +101,16 @@ const Editor = () => {
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은?" />
+        <textarea
+          placeholder="오늘은?"
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+        />
       </section>
       <section className="button_section">
-        <Button text="취소" />
-        <Button text="작성" type="positive" />
+        <Button text="취소" onClick={() => nav(-1)} />
+        <Button text="작성" type="positive" onClick={onClickSubmitButton} />
       </section>
     </div>
   );
--- react-project-3/src/pages/Edit.jsx
+++ react-project-3/src/pages/Edit.jsx
@@ -1,8 +1,60 @@
-import { useParams } from "react-router-dom";
-
+import { useParams, useNavigate, replace } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id} 일기다.</div>;
+  const nav = useNavigate();
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
+  const onClickDelete = () => {
+    const res = window.confirm("진짜?");
+    if (res) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  const [curDiaryItem, setCurDiaryItem] = useState();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (i) => String(i.id) === String(params.id)
+    );
+    if (!currentDiaryItem) {
+      window.alert("!!!!!!!!!!!!!!");
+      nav("/", { replace: true });
+    }
+
+    setCurDiaryItem(currentDiaryItem);
+  }, [params.id, data]);
+
+  const onSubmit = (input) => {
+    if (window.confirm("수정?")) {
+      onUpdate(
+        input.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title="수정"
+        leftChild={<Button text={"< 뒤로가기"} onClick={() => nav(-1)} />}
+        rightChild={
+          <Button text={"삭제하기"} type="negative" onClick={onClickDelete} />
+        }
+      />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Edit;
