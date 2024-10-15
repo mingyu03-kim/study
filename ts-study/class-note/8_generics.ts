79bbedd49d55220c971601a504ad4c03971c9bb6
mingyu03-kim <kimmingyu65@gmail.com>
2024-10-16 06:16:17 +0900

    🐛 제네릭을 쓰지 않으면, 함수로는 중복 선언이 필요하다.

--- ts-study/class-note/8_generics.ts
+++ ts-study/class-note/8_generics.ts
@@ -17,3 +17,27 @@ logText('하이');
 
 // 직접 명시해도 된다. 이 경우 해당 타입으로만 넘길 수 있음.
 logText<string>('하이');
+
+
+
+// 제네릭의 장점?
+// => 함수로는 계속 타입을 다른 것이라고 선언해줘야 하므로
+// 중복 선언이 필요하다.
+function logText1(text: string) {
+    console.log(text);
+    text.split('').reverse().join();
+    return text;   
+}
+
+logText1('a');
+logText1(10);
+logText1(true);
+
+function logNumber(text: number) {
+    console.log(text);
+    return text;   
+}
+
+logNumber('a');
+logNumber(10);
+logNumber(true);
