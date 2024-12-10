df9e14e3a8aa510e57e43b485f5f3756d03fc388
mingyu03-kim <kimmingyu65@gmail.com>
2024-12-10 22:32:17 +0900

    🔨 SSR 사용실습. getServerSideProps

--- FE/react/nextjs/onebite-next/section02/src/pages/index.tsx
+++ FE/react/nextjs/onebite-next/section02/src/pages/index.tsx
@@ -4,40 +4,33 @@ import style from './index.module.css';
 import books from '@/mock/books.json';
 import BookItem from '@/components/book-item';
 import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
 // Next에서 사용하는 메서드. SSR로 인식됨
-export const getServerSideProps = () => {
-  // component보다 먼저 실행된다.
-  const data = "hello";
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(), fetchRandomBooks(),
+  ])
 
-  console.log('이건 서버에서 실행되기 때문에 브라우저 콘솔에 찍히지 않는다.', data);
-  // window.location 같은, 브라우저 값을 이용하지 못함.
   return { // 반드시 객체를 반환해야 하며,
     props: { // 반드시 props를 반환해야 한다.
-      data,
+      allBooks,
+      recoBooks
     }
   }
 };
 
-export default function Home({data}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // 이제는 SSR방식이기 때문에, 서버에서도 이 컴포넌트가 한번 실행된다. 
-  // 그래서 그냥 사용하면 서버에서 에러가 발생.
-  // window.location 
-  // 따라서 mounted 됐을 때 사용하는 방식으로 해야 함.
-  useEffect(() => {
-    console.log(window);
-  }, []);
-
-  console.log('???', data);
+export default function Home({allBooks, recoBooks}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => <BookItem key={book.id} {...book} />)}
+        {recoBooks.map((book) => <BookItem key={book.id} {...book} />)}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => <BookItem key={book.id} {...book} />)}
+        {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
       </section>
     </div>
   );
--- FE/react/nextjs/onebite-next/section02/src/pages/search/index.tsx
+++ FE/react/nextjs/onebite-next/section02/src/pages/search/index.tsx
@@ -1,11 +1,21 @@
 import SearchableLayout from "@/components/searchable";
 import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import books from '@/mock/books.json';
 import BookItem from "@/components/book-item";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+    const q = context.query.q;
+    const books = await fetchBooks(q as string);
+    return {
+        props: {
+            books
+        },
+    }
+}
 
-export default function Page() {
+export default function Page({books}:InferGetServerSidePropsType<typeof getServerSideProps>) {
     return <div>
         {books.map((book) => <BookItem key={book.id} {...book} />)}
     </div>;
