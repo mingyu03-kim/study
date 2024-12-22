883b7649d667c13aeda09119a4cd6c86f959078d
mingyu03-kim <kimmingyu65@gmail.com>
2024-12-22 18:43:31 +0900

    🔨 App router에서의 data fetching -> 이제 필요한 페이지에서 각자 페칭.

--- FE/react/nextjs/onebite-next/section03/src/app/(with-searchbar)/page.tsx
+++ FE/react/nextjs/onebite-next/section03/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,57 @@
 import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+
+  if (!response.ok) return <div>오류,,,</div>;
+
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+      ;
+    </div>
+  );
+}
+
+async function RecoBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
+
+  if (!response.ok) return <div>오류,,,</div>;
+
+  const recoBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {recoBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+      ;
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        {/* @ts-expect-error Server Component */}
+        <RecoBooks />
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        {/* @ts-expect-error Server Component */}
+        <AllBooks />
       </section>
     </div>
   );
