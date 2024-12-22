df92c4e49b0c1f8326b86c05091759d5ba203fa1
mingyu03-kim <kimmingyu65@gmail.com>
2024-12-22 16:45:30 +0900

    🚨 Server/Client 컴포넌트를 사용할 때의 주의점. client에서는 server를 import하면 server 컴포넌트가 자동으로 client로 변환되니, children을 사용하는 방식으로 사용해야 함.

--- FE/react/nextjs/onebite-next/section03/src/app/(with-searchbar)/page.tsx
+++ FE/react/nextjs/onebite-next/section03/src/app/(with-searchbar)/page.tsx
@@ -1,6 +1,8 @@
 // "use client"; // client 컴포넌트로 선언하는 코드.
 
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
+import ServerComponent from "./server-component";
 
 export default function Home() {
   // console.log("Home 컴포넌트 실행");
@@ -8,5 +10,12 @@ export default function Home() {
   // 브라우저에서만 동작하기 때문에, 해당 훅 사용 불가.
   // useEffect(() => {});
 
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
+    </div>
+  );
 }
