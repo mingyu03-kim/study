9a4f9957c7a560d3c543e33e3d3daf15fa34bc1a
mingyu03-kim <kimmingyu65@gmail.com>
2024-12-22 16:23:31 +0900

    💡 server, client 2번 실행되기 때문에, client 전용으로만 실행하게 하려면 "use client"를 작성해야 함.

--- FE/react/nextjs/onebite-next/section03/src/app/(with-searchbar)/page.tsx
+++ FE/react/nextjs/onebite-next/section03/src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,12 @@
+// "use client"; // client 컴포넌트로 선언하는 코드.
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  // console.log("Home 컴포넌트 실행");
+
+  // 브라우저에서만 동작하기 때문에, 해당 훅 사용 불가.
+  // useEffect(() => {});
+
   return <div className={styles.page}>인덱스 페이지</div>;
 }
